9b1474a03f1d64fc7dc5e891d0a641700bbe8984
DESKTOP-1NGUJ0F\admin <arkanum3608@naver.com>
2022-03-03 10:46:45 +0900

    0303

--- manage.py
+++ manage.py
@@ -4,7 +4,6 @@ from datetime import datetime
 import json
 import requests
 
-current_time = datetime.now()
 
 app = Flask(__name__)
 app.secret_key ='super secret key'
@@ -36,16 +35,24 @@ def login():
             else:
                 return "아이디가 없습니다."
         except:
-            return "don't login"
+            return "Error"
     else:
         return render_template('my_login.html')
     
-@app.route("/main",methods=["POST","GET"])
-def main():
-    return render_template('my_prj_test_1.html',time= current_time)
 
 @app.route("/main",methods=["POST","GET"])
 def piechart():
+    current_time = datetime.now()
+    apikey =""
+    city_list =["Seoul,KR"]
+    api = "http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={key}"
+    k2C = lambda k: k -273.15
+
+    for name in city_list:
+        url = api.format(city=name, key=apikey)
+        res = requests.get(url)
+        data = json.loads(res.text)
+    
     con = m.connect(host="localhost",
                 user = "root", passwd= "dxa132",
                 db = "dashproject1", charset= "utf8")
@@ -71,17 +78,24 @@ def piechart():
                 early_leave_cnt +=1;
             else:
                 absent_cnt +=1;
-    all_list={
+    colors = ['#b2c831','#c4d468','#f8f8f8', '#151515']
+    pie_list={
         "labels": ['출석', '지각', '비대면', '결석'],
         "datasets": [{
-            "backgroundColor": "colors".slice(0,4),
+            "backgroundColor": colors[0:4],
             "borderWidth": 0,
             "data": [attend_cnt,late_attend_cnt,untact_cnt,absent_cnt] }]
     }
-    return render_template('my_prj_test_1.html',data_list = json.dump(all_list))
+    all_list = {}
+    all_list["pie_list"] =pie_list
+    all_list["temperature"] = [data["name"], data["weather"][0]["description"], round(k2C(data["main"]["temp_min"]),2),round(k2C(data["main"]["temp_max"]),2)]
+    all_list["time"] = current_time
+    
+    return render_template('my_prj_test_1.html',data_list = all_list)
 
 
-
+if __name__ == "__main__":
+    app.run(host="localhost", port=5000)
 
 
 apikey ="1c98556bab77208b58bc7cf6168ff0bb"
